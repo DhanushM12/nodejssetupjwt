0483e034ac2b9391359beae0e277c28fed6b41f5
DhanushM12 <dhanushmukhi@gmail.com>
2023-08-09 21:58:36 +0530

    generate token

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const app = express();
 const port = 8080;
+const jwt = require('jsonwebtoken');
 
 app.get('/', (req, res) => {
     res.json({
         "message": "Welcome to Node js"
     })
     // res.send('<h1>Hello Welcome to Node js!!!!</h1>')
+})
+
+app.post('/tokenGenerate', (req, res) => {
+    const user = {
+        id: 998983,
+        username: 'Jan2023',
+        email: 'jan23@coding.com'
+    }
+    jwt.sign(user, 'secret', function(err, token) {
+        if(err){
+            res.statusCode(403);
+        } else {
+            res.json({
+                token
+            })
+        }
+      });
 })
 
 app.listen(port, function(err){
