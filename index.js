45cd639877b3615c5db7af57922c912e03b74f92
DhanushM12 <dhanushmukhi@gmail.com>
2023-08-10 22:22:37 +0530

    Added jwt verify

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ app.get('/', (req, res) => {
 app.post('/tokenGenerate', (req, res) => {
     const user = {
         id: 998983,
-        username: 'Jan2023',
+        username: 'Jan20231',
         email: 'jan23@coding.com'
     }
-    jwt.sign(user, 'secret', function(err, token) {
+    jwt.sign(user, 'secret', {expiresIn: '60s'}, function(err, token) {
         if(err){
             res.statusCode(403);
         } else {
@@ -26,6 +26,32 @@ app.post('/tokenGenerate', (req, res) => {
         }
       });
 })
+
+app.post('/verifyToken', extractToken, (req, res) => {
+    jwt.verify(req.token, 'secret', function(err, data) {
+        if(err){
+            res.sendStatus(403);
+        } else {
+            res.json({
+                message: 'User access granted',
+                info: data
+            })
+        }
+      });
+})
+
+// middleware
+function extractToken(req, res, next) {
+    const bearerHeader = req.headers['authorization']; //"Bearer token"
+    if(bearerHeader !== undefined){
+        const bearer = bearerHeader.split(' '); // ['Bearer', token]
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next()
+    } else {
+        res.sendStatus(403);
+    }
+}
 
 app.listen(port, function(err){
     if(err){
